74008c4788c0bd6340b9f35aeda016047bfe0c42
rs-benatti <rafa.senna.benatti2@gmail.com>
2022-10-24 14:27:59 +0200

    adding commentaries to main file

--- src/main.py
+++ src/main.py
@@ -12,7 +12,8 @@ import kmeans
 import space_transformation
 import clustering
 
-def retrieve_images(lesion_type): # 'melanoma' ou 'nevus'
+# This function returns a list of io images from the group passed as parameter ('melanoma' or 'nevus')
+def retrieve_images(lesion_type): # Parameter: 'melanoma' or 'nevus'
     im = []
     for f in os.listdir('./src/images/' + lesion_type):
         name, extension = os.path.splitext(f)
@@ -23,6 +24,8 @@ def retrieve_images(lesion_type): # 'melanoma' ou 'nevus'
             pass
     return im
 
+# This function returns an array of segmented io images from the group passed as parameter ('melanoma' or 'nevus')
+# This function is not being used in code yet, it's going to be used further
 def retrieve_segmentations(lesion_type):
     im = []
     for f in os.listdir('./src/images/'+ lesion_type):
@@ -31,7 +34,8 @@ def retrieve_segmentations(lesion_type):
         im.append(io.imread('src\images\\' + lesion_type + '\ISIC_'+id+'_Segmentation.jpg'))
     return im
 
-def get_id_list(lesion_type): # 'melanoma' ou 'nevus'
+# This function returns a list of the id of the images gro the group passed as parameter
+def get_id_list(lesion_type): # 'melanoma' or 'nevus'
     id_list = []
     for f in os.listdir('./src/images/' + lesion_type):
         name, extension = os.path.splitext(f)
@@ -43,33 +47,46 @@ def get_id_list(lesion_type): # 'melanoma' ou 'nevus'
 
     return id_list
 
+
+# This is the main function.
 def main():
     im = retrieve_images('melanoma')
-    #main_task(im, 2)
     processlist = []
+
+    # The code block below serves only to append the parallel processes
     for img_index in range(0, len(im)):
         processlist.append(Process(target=main_task, args=(im, img_index, 'melanoma')))
-
     im = retrieve_images('nevus')
     for img_index in range(0, len(im)):
         processlist.append(Process(target=main_task, args=(im, img_index, 'nevus')))
-
     for process in processlist:
         process.start()
-
     for process in processlist:
         process.join()
 
+# This code is currently using parallel processes, so, the main function calls the function below.
 def main_task(img_array, img_index, lesion_type):
     id = get_id_list(lesion_type)
     r = 3
     num = 20 * r
-    Y, lbp = LBP_processing.lbp(img_array[img_index], r, num)
-    Y, binary_lbp = LBP_processing.binary_lbp(img_array[img_index], r, num)
+
+    # This implementation returns the Y scale of the original image without the application of a gaussian filter and
+    # the image itself after a LBP, binarization and application of a gaussian filter
     Y, gaussian_lbp = LBP_processing.gaussian_lbp(img_array[img_index], r, num)
+
+    # This function returns the LBP treated image in a LAB color space
     lab_image = space_transformation.lab_color_space(Y, gaussian_lbp)
+
+    # This fucntion returns the image after the application of kmeans.
+    # It is evident that this is not optimal, we must try to apply the kmeans algorithm only one time.
     clustered = kmeans.kmeans(kmeans.cluster(kmeans.kmeans_colors(lab_image, 2)), 2)
-    clustered = clustering.create_mask(clustered, thresh=0.89, rayon=50, x0=clustered.shape[1]//2, y0=clustered.shape[1]//2)
+
+    # The commented line below was being used to reduce the size of the image.
+    # Professor Pietro confirmed that there was no problem about it.
+
+    #clustered = clustering.create_mask(clustered, thresh=0.89, rayon=50, x0=clustered.shape[1]//2, y0=clustered.shape[1]//2)
+
+    # The line below is used to save the images inside the folder ./out
     io.imsave('out/'+ lesion_type + '/mask_' + id[img_index] + '.png', clustered)
 
 if __name__ == "__main__":
