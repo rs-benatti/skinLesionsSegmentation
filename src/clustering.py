8fe106dd23b6a958fc020ec763218e9cef4e552e
rs-benatti <rafa.senna.benatti2@gmail.com>
2022-11-02 14:53:15 +0100

    masks inversion correction

--- src/clustering.py
+++ src/clustering.py
@@ -2,6 +2,7 @@ import skimage.morphology as morpho
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2
+import random
 
 def my_perimeter(im):
     se=morpho.disk(1)
@@ -10,12 +11,33 @@ def my_perimeter(im):
     ta=np.nonzero(diff)
     return ta
 
-def create_mask(im, thresh=0.85, rayon=9, x0=300, y0=300):
-    mask=np.zeros((len(im),len(im[0])))
-    mask[y0,x0]=255
-    ext=im[y0-rayon:y0+rayon+1,x0-rayon:x0+rayon+1]
-    m0=np.mean(ext)
-    s0=np.std(ext)
+def create_mask(im, thresh=0.85, rayon=9, show_clustering=0):
+    m0 = im[:, :].max()
+    iterCounter = 0
+    while ((m0 > (0.2 * im[:, :].max()) or s0 == 0) or np.isnan(m0) == True):
+        center = get_center_pixel(im)
+        x0 = center[0]
+        y0 = center[1]
+        mask=np.zeros((len(im[0]),len(im[1])))
+        mask[y0,x0]=255
+        ext=im[y0-rayon:y0+rayon+1,x0-rayon:x0+rayon+1]
+        m0=np.mean(ext)
+        s0=np.std(ext)
+        if iterCounter > 1000:
+            print("Reached stop condition")
+            x0 = im.shape[1] // 2
+            y0 = im.shape[0] // 2
+            mask=np.zeros((len(im[0]),len(im[1])))
+            mask[y0,x0]=255
+            ext=im[y0-rayon:y0+rayon+1,x0-rayon:x0+rayon+1]
+            m0=np.mean(ext)
+            s0=np.std(ext)
+            print(m0)
+            print(s0)
+            break
+        print(m0)
+        print(s0)
+        iterCounter += 1
     modif=1
     iter = 0 
     while modif >  0:
@@ -31,6 +53,10 @@ def create_mask(im, thresh=0.85, rayon=9, x0=300, y0=300):
             if np.abs(m0-m) < thresh * s0 :
                 mask[y][x]=255
                 modif=1
+        if(show_clustering):
+            cv2.imshow('frame',mask)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
     return mask
 
 def plot_superposition(im, mask):
@@ -38,3 +64,23 @@ def plot_superposition(im, mask):
     plt.imshow(im, cmap='gray') # I would add interpolation='none'
     plt.imshow(mask, cmap='jet', alpha=0.5) # interpolation='none'
     plt.show()
+
+# Check if the pixel in the center of the image is part of the lesion (wether it's black)
+# If it's not, searchs for a black pixel in the image
+# Returns a tuple with the  pixel coordinates
+def get_center_pixel(im):
+    print("Called function get_center_pixel")
+    x0 = im.shape[0]//2
+    y0 = im.shape[1]//2
+    center=(x0, y0)
+    centerPixel  = im[x0][y0]
+    threshold = 0.8 * im[:, :].max()
+    minimum = im[:, :].min()
+    side = int(0.4 * min([im.shape[0], im.shape[1]]))
+    print(im.shape)
+    while (centerPixel != minimum):
+        x0 = random.randint(0, im.shape[0] - 1)
+        y0 = random.randint(0, im.shape[1] - 1)
+        centerPixel  = im[x0][y0]
+        center=(x0, y0)
+    return center
--- src/main.py
+++ src/main.py
@@ -13,15 +13,13 @@ import space_transformation
 import clustering
 
 # This function returns a list of io images from the group passed as parameter ('melanoma' or 'nevus')
-def retrieve_images(lesion_type, resized = 1): # Parameter: 'melanoma' or 'nevus'
+def retrieve_images(lesion_type): # Parameter: 'melanoma' or 'nevus'
     im = []
     for f in os.listdir('./src/images/' + lesion_type):
         name, extension = os.path.splitext(f)
         try:
             id = str(re.findall("(\d+)$", name)[0])
             image = io.imread('src\images\\' + lesion_type + '\ISIC_'+id+'.jpg')
-            if (resized):
-                image = resize(image, image.shape[0] // 3, image.shape[1] //3)
             im.append(image)
         except:
             pass
@@ -53,7 +51,11 @@ def get_id_list(lesion_type): # 'melanoma' or 'nevus'
 
 # This is the main function.
 def main():
+    
     im = retrieve_images('melanoma')
+    main_task(im, 1, 'melanoma')
+
+    '''
     processlist = []
 
     # The code block below serves only to append the parallel processes
@@ -66,7 +68,7 @@ def main():
         process.start()
     for process in processlist:
         process.join()
-
+    '''
 # This code is currently using parallel processes, so, the main function calls the function below.
 def main_task(img_array, img_index, lesion_type):
     id = get_id_list(lesion_type)
@@ -84,10 +86,14 @@ def main_task(img_array, img_index, lesion_type):
     # It is evident that this is not optimal, we must try to apply the kmeans algorithm only one time.
     clustered = kmeans.kmeans(kmeans.cluster(kmeans.kmeans_colors(lab_image, 2)), 2)
 
-    clustered = clustering.create_mask(clustered, thresh=0.89, rayon=50, x0=clustered.shape[1]//2, y0=clustered.shape[1]//2)
+    resizingFactor = 3 # to don't resize, use 1
+    clustered = resize(clustered, (clustered.shape[0]//resizingFactor, clustered.shape[1]//resizingFactor))
+    plt.imshow(clustered, cmap='gray')
+    plt.show()
+    clustered = clustering.create_mask(clustered, thresh=0.91, rayon=50//(resizingFactor), show_clustering=1)
 
     # The line below is used to save the images inside the folder ./out
     io.imsave('out/'+ lesion_type + '/mask_' + id[img_index] + '.png', clustered)
-
+    print(f'Saved image with id: {id[img_index]}')
 if __name__ == "__main__":
     main()
